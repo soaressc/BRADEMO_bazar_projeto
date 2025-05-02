01eba6f49f281289dcf0f0cae779defaae4f5d70
anaangelieri <131811398+anaangelieri@users.noreply.github.com>
2025-05-02 16:02:07 -0300

    adicionando o model de book

---
+++ lib/models/book.dart
@@ -0,0 +1,9 @@
+//criando a classe do livro, com titulo, preco e o caminho da imagem
+class Book{
+  final String titulo;
+  final String preco;
+  final String caminhoImagem;
+
+  Book({required this.titulo, required this.preco, required this.caminhoImagem});
+  
+}
