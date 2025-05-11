d4f8099affeb505328aec1260fc22511ee2e0586
Giovanna Laura <103960436+giovannalauraa@users.noreply.github.com>
2025-05-11 18:27:39 -0300

    Adicionando a rota para o carrinho no main

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:myapp/screens/auth/signup_screen.dart';
 import 'package:myapp/screens/home.dart';
 import 'package:myapp/screens/onboarding_screen.dart';
 import 'package:myapp/screens/splash_screen.dart';
+import 'package:myapp/screens/cart_empty_screen.dart'; 
 
 void main() {
   runApp(const MyApp());
@@ -21,6 +22,7 @@ class MyApp extends StatelessWidget {
         '/onboarding': (context) => const OnboardingScreen(),
         '/home': (context) => const Home(),
         '/signup': (context) => const SignUpScreen(),
+        '/cart': (context) => const CartEmptyScreen(),
       },
     );
   }
