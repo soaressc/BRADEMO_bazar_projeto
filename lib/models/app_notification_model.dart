c5627c336e1d34f64a8b0dfeb6f23b55b5cb829f
Giovanna Laura <103960436+giovannalauraa@users.noreply.github.com>
2025-06-04 18:13:07 -0300

    Iniciando a implementação da  notification screen

---
+++ lib/models/app_notification_model.dart
@@ -0,0 +1,15 @@
+class AppNotification {
+  // final String id;
+  final String titulo;
+  final String mensagem;
+  final bool lida;
+  final DateTime data;
+
+  AppNotification({
+    // required this.id,
+    required this.titulo,
+    required this.mensagem,
+    required this.lida,
+    required this.data,
+  });
+}
