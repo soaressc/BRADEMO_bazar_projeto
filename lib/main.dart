8182044e57a2bbb0d4492b77b2d665cbf8877f4f
Ana Laura <131811398+anaangelieri@users.noreply.github.com>
2025-06-09 16:16:17 -0300

    Inicializando o Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/firebase_options.dart';
 import 'package:myapp/screens/auth/signup_screen.dart';
 import 'package:myapp/screens/home.dart';
 import 'package:myapp/screens/onboarding_screen.dart';
@@ -6,7 +8,11 @@ import 'package:myapp/screens/splash_screen.dart';
 import 'package:myapp/screens/cart_empty_screen.dart';
 import 'package:myapp/screens/notification_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
   runApp(const MyApp());
 }
 
