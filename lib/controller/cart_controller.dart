2c87df1161203cce0cd5cbe0fe65bc904463c998
sosocsoares <sosocsoares@gmail.com>
2025-06-12 17:27:31 +0000

    [Cart] Service, Repository and Controller Refactor

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,25 +1,52 @@
-// Cart Controller
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:myapp/models/book.dart';
 import 'package:myapp/models/cart.dart';
 import 'package:myapp/repository/cart_repository.dart';
 
-class CartController {
-  final CartRepository cartRepository;
+class CartController extends ChangeNotifier {
+  final CartRepository _cartRepository = CartRepository();
+  late Future<Cart?> _futureCart;
+  String? userId;
 
-  CartController({required this.cartRepository});
-
-  Future<void> createCart(Cart cart) async {
-    await cartRepository.addCart(cart);
+  CartController() {
+    final user = FirebaseAuth.instance.currentUser;
+    userId = user?.uid;
+    if (userId != null) {
+      _futureCart = _cartRepository.getCartWithItems(userId!);
+    }
   }
 
-  Future<Cart?> getCart(String id) async {
-    return await cartRepository.fetchCart(id);
+  Future<void> fetchCart() async {
+    if (userId != null) {
+      _futureCart = _cartRepository.getCartWithItems(userId!);
+      notifyListeners();
+    }
   }
 
-  Future<void> updateCart(Cart cart) async {
-    await cartRepository.updateCart(cart);
+  Future<void> removeItem(String cartItemId) async {
+    final cart = await _futureCart;
+    if (cart != null) {
+      await _cartRepository.removeItem(cart.id, cartItemId);
+      fetchCart();
+    }
   }
 
-  Future<void> deleteCart(String id) async {
-    await cartRepository.removeCart(id);
+  Future<void> updateQuantity(String cartItemId, int newQuantity) async {
+    final cart = await _futureCart;
+    if (cart != null) {
+      final item = cart.items.firstWhere((i) => i.id == cartItemId);
+      final updatedItem = item.copyWith(quantity: newQuantity);
+      await _cartRepository.removeItem(cart.id, updatedItem.id);
+      fetchCart();
+    }
+  }
+
+  Future<Map<String, Book>> fetchBooks(List<String> bookIds) async {
+    return await _cartRepository.fetchBooks(bookIds);
+  }
+
+  Future<Cart?> getCart() async {
+    return await _futureCart;
   }
 }
